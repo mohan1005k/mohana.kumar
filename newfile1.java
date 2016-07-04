f8e39bbaf3cae8dcc07958ce19a3258903c4c84a
mohan <mohan1005k@gmail.com>
2016-07-04 13:56:38 +0000

    changes

--- newfile1.java
+++ newfile1.java
@@ -3,6 +3,6 @@ import java.io.*
 public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "mohan";
-        System.out.println("change 1  " + a + "!");
+        System.out.println("change 2 " + a + "!");
     }
 }
