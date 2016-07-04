efbfe214271c283453cc848a46fb76d8c8d86ee4
mohan1005k <mohan1005k@gmail.com>
2016-07-04 17:36:19 +0400

    Update newfile1.java

--- newfile1.java
+++ newfile1.java
@@ -3,6 +3,6 @@ import java.io.*
 public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "mohan";
-        System.out.println("Hello World " + a + "!");
+        System.out.println("change 1  " + a + "!");
     }
 }
